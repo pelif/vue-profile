feb948ce263c89a704bf232e5fb887a5014c031d
Felipe Daniel <felipe.dandev@gmail.com>
2024-03-19 21:33:29 -0300

    config tailwind and navbar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,9 @@
 <template>
+  <Transition name="fade">
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+</Transition>
 </template>
 
 <style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
     theme: {
+      extend: {},
+    },
+    variants: {
       extend: {},
     },
     plugins: [],
   }
+  
+  
